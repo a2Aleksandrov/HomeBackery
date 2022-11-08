ec5ac1c84a753f423edd9347507694b2427a77b8
ALeksandrov <a3alex89@gmail.com>
2022-11-08 17:07:33 +0200

    added logic for deleting completed orders

--- controllers/allOrdersCtrl.js
+++ controllers/allOrdersCtrl.js
@@ -1,15 +1,20 @@
-const { getAllOrders } = require('../services/orderService');
+const { getAllOrders, deleteOrder } = require('../services/orderService');
 
 const ordersListController = require('express').Router();
 
 ordersListController.get('/', async (req, res) => {
     const orders = await getAllOrders().lean();
+
     res.render('ordersList', {
         title: 'All Orders',
         user: req.user,
         orders
     });
-    console.log(orders);
+});
+
+ordersListController.get('/:id/delete', async (req, res) => {
+    await deleteOrder(req.params.id);
+    res.redirect('/ordersList');
 });
 
 module.exports = ordersListController;
